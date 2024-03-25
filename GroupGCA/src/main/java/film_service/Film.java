ff9b39844a9a190f5e2ed9bccc3660bb46c15718
Smitza <146719967+Smitza@users.noreply.github.com>
2024-03-25 17:17:35 +0000

    Film class

--- GroupGCA/src/main/java/film_service/Film.java
+++ GroupGCA/src/main/java/film_service/Film.java
@@ -1,5 +1,62 @@
 package film_service;
 
-public class Film {
+import java.util.Objects;
 
+public class Film {
+    private String title;
+    private String genre;
+    private int totalRatings;
+    private int numRatings;
+
+    public Film(String title, String genre, int totalRatings, int numRatings) {
+        this.title = title;
+        this.genre = genre;
+        this.totalRatings = totalRatings;
+        this.numRatings = numRatings;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getTotalRatings() {
+        return totalRatings;
+    }
+
+    public void setTotalRatings(int totalRatings) {
+        this.totalRatings = totalRatings;
+    }
+
+    public int getNumRatings() {
+        return numRatings;
+    }
+
+    public void setNumRatings(int numRatings) {
+        this.numRatings = numRatings;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Film film = (Film) o;
+        return Objects.equals(title, film.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, genre, totalRatings, numRatings);
+    }
 }

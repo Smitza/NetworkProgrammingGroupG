2cbfbbd5e548651692c2a75675b775da0a50624d
Smitza <146719967+Smitza@users.noreply.github.com>
2024-04-22 20:30:29 +0100

    Added some handling for the server responses

--- FilmClient/src/main/java/client/FilmClient.java
+++ FilmClient/src/main/java/client/FilmClient.java
@@ -2,13 +2,10 @@ package client;
 
 import service.FilmService;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FilmClient {
@@ -19,68 +16,137 @@ public class FilmClient {
             try (Scanner input = new Scanner(dataSocket.getInputStream());
                  PrintWriter output = new PrintWriter(dataSocket.getOutputStream())) {
                 boolean validSession = true;
+                boolean isAdmin = false;
+                boolean isLoggedIn = false;
 
+                //Menu for Client
                 while (validSession) {
-                    System.out.println("Enter your message ('exit' to quit)");
-                    String message = generateRequest(userInput);
-                    output.println(message);
-                    output.flush();
-                    String response = input.nextLine();
-                    switch(response) {
-                        case FilmService.ADD:
-                            System.out.println("it had been added successfully");
-                            break;
-                        case FilmService.REJECTED:
-                            System.out.println("your item does not meet the criteria");
-                            break;
-                        case FilmService.SUCCESSADMIN:
-                            System.out.println("you have been logged in as an admin ");
-                            break;
-                        case FilmService.SUCCESSUSER:
-                            System.out.println("you have been logged in as a user");
-                            break;
-                        case FilmService.LOGOUTOUT:
-                            System.out.println("you have been logged out succesfully");
-                            break;
-                        case FilmService.FAILED:
-                            System.out.println("you have failed to login in succesfully");
-                            break;
-                        case FilmService.SUCCESS:
-                            System.out.println("you have been logged in succesfully ");
-                            break;
-                        case FilmService.EXIT:
-                            System.out.println("it does  currently exist");
-                            break;
-                        case FilmService.REMOVE:
-                            System.out.println("the current item have been removed");
-                            break;
-                        case FilmService.NOTFOUND:
-                            System.out.println("the current item was not found");
-                            break;
-                        case FilmService.INVALIDRATING:
-                            System.out.println("you have entered an invalid rating");
-                            break;
-                        case FilmService.NOLOGIN:
-                            System.out.println("there is no results in the login");
-                            break;
-                        case FilmService.NOMATCH:
-                            System.out.println("there is no match that is added into it ");
-                            break;
-                        case FilmService.INVALID:
-                            System.out.println("the criteria added is invalid");
-                            break;
-                        case FilmService.SHUTDOWN:
-                            System.out.println("you have successfully shut down");
-                            break;
-                        case FilmService.NOPERMS:
-                            System.out.println("you do not have permissions to access");
-                            break;
-                        case FilmService.GOODBYE:
-                            System.out.println("you have succesfully left the section ");
-                            break;
+                    if (!isLoggedIn) {
+                        System.out.println("Enter your choice:");
+                        System.out.println("1. Register");
+                        System.out.println("2. Login");
+                        System.out.println("3. Search film by name");
+                        System.out.println("4. Search film by genre");
+                        System.out.println("5. Exit");
+                    } else {
+                        System.out.println("Enter your choice:");
+                        System.out.println("3. Search film by name");
+                        System.out.println("4. Search film by genre");
+                        System.out.println("5. Exit");
+                        System.out.println("6. Rate a film");
+                        System.out.println("7. Logout");
+                        if (isAdmin) {
+                            System.out.println("8. Add a film (admin only)");
+                            System.out.println("9. Remove a film (admin only)");
+                            System.out.println("10. Shutdown (admin only)");
+                        }
                     }
-                    System.out.println("Received: " +response);
-                    if(response.equals(FilmService.GOODBYE)){
+
+                    String choice = userInput.nextLine();
+                    String request = null;
+
+                    //Client requests
+
+                    switch (choice) {
+                        case "1":
+                            if (!isLoggedIn) {
+                                System.out.println("Please enter your username to register: ");
+                                String regusername = userInput.nextLine();
+                                System.out.println("Please enter your password to register: ");
+                                String regpassword = userInput.nextLine();
+                                request = FilmService.REGISTER + FilmService.DELIMITER + regusername + FilmService.DELIMITER + regpassword;
+                            } else {
+                                System.out.println("You are already logged in!");
+                            }
+                            break;
+                        case "2":
+                            if (!isLoggedIn) {
+                                System.out.println("Please enter your username to login: ");
+                                String username = userInput.nextLine();
+                                System.out.println("Please enter your password to login: ");
+                                String password = userInput.nextLine();
+                                request = FilmService.LOGIN + FilmService.DELIMITER + username + FilmService.DELIMITER + password;
+                            } else {
+                                System.out.println("You are already logged in!");
+                            }
+                            break;
+                        case "3":
+                            System.out.println("Enter Title: ");
+                            String searchTitle = userInput.nextLine();
+                            request = FilmService.SEARCHNAME + FilmService.DELIMITER + searchTitle;
+                            break;
+                        case "4":
+                            System.out.println("Enter Genre: ");
+                            String searchGenre = userInput.nextLine();
+                            request = FilmService.SEARCHGENRE + FilmService.DELIMITER + searchGenre;
+                            break;
+                        case "5":
+                            request = FilmService.EXIT;
+                            break;
+                        case "6":
+                            if (isLoggedIn) {
+                                System.out.println("Title of film to rate: ");
+                                String rateTitle = userInput.nextLine();
+                                System.out.println("Rating out of 10: ");
+                                String rating = userInput.nextLine();
+                                request = FilmService.RATE + FilmService.DELIMITER + rateTitle + FilmService.DELIMITER + rating;
+                            } else {
+                                System.out.println("Please select one of the options");
+                                System.out.println("=========================================================");
+                            }
+                            break;
+                        case "7":
+                            if (isLoggedIn) {
+                                request = FilmService.LOGOUT;
+                            } else {
+                                System.out.println("Please select one of the options");
+                                System.out.println("=========================================================");
+                            }
+                            break;
+                        case "8":
+                            if (isAdmin) {
+                                System.out.println("Enter title of Film: ");
+                                String filmTitle = userInput.nextLine();
+                                System.out.println("Enter genre of Film: ");
+                                String filmGenre = userInput.nextLine();
+                                request = FilmService.ADD + FilmService.DELIMITER + filmTitle + FilmService.DELIMITER + filmGenre;
+                            } else {
+                                System.out.println("Please select one of the options");
+                                System.out.println("=========================================================");
+                            }
+                            break;
+                        case "9":
+                            if (isAdmin) {
+                                System.out.println("Enter title of Film to remove: ");
+                                String removeTitle = userInput.nextLine();
+                                request = FilmService.REMOVE + FilmService.DELIMITER + removeTitle;
+                            } else {
+                                System.out.println("Please select one of the options");
+                                System.out.println("=========================================================");
+                            }
+                            break;
+                        case "10":
+                            if (isAdmin) {
+                                request = FilmService.SHUTDOWN;
+                                validSession = false; // Exit the loop immediately after sending the shutdown request
+                            } else {
+                                System.out.println("Please select one of the options");
+                                System.out.println("=========================================================");
+                            }
+                            break;
+                        default:
+                            System.out.println("Please select one of the options");
+                            System.out.println("=========================================================");
+                            continue;
+                    }
+                    output.println(request);
+                    output.flush();
+
+                    String response = input.nextLine();
+                    isAdmin = processResponse(response);
+                    isLoggedIn = response.equals(FilmService.SUCCESSADMIN) || response.equals(FilmService.SUCCESSUSER);
+                    if (response.equals(FilmService.GOODBYE)) {
+                        System.out.println("Goodbye!");
                         validSession = false;
                     }
                 }
@@ -92,89 +158,63 @@ public class FilmClient {
         }
     }
 
-
-    public static void displayMenu() {
-        System.out.println("Enter your choice:");
-        System.out.println("1. Register");
-        System.out.println("2. Login");
-        System.out.println("3. Rate a film (Needs login)");
-        System.out.println("4. Search film by name");
-        System.out.println("5. Search film by genre");
-        System.out.println("6. Add a film (admin only)");
-        System.out.println("7. Remove a film (admin only)");
-        System.out.println("8. Shutdown (admin only)");
-        System.out.println("9. Logout");
-        System.out.println("10. Exit");
-    }
-
-    public static String generateRequest(Scanner userInput) {
-        boolean valid = false;
-        String request = null;
-
-        while (!valid) {
-            displayMenu();
-            String choice = userInput.nextLine();
-            int value = 0;
-            switch (choice) {
-                case "1":
-                    System.out.println("Please enter your username to register: ");
-                    String regusername = userInput.nextLine();
-                    System.out.println("Please enter your password to register: ");
-                    String regpassword = userInput.nextLine();
-                    request = FilmService.REGISTER + FilmService.DELIMITER + regusername + FilmService.DELIMITER + regpassword;
-                    break;
-                case "2":
-                    System.out.println("Please enter your username to login: ");
-                    String username = userInput.nextLine();
-                    System.out.println("Please enter your password to login: ");
-                    String password = userInput.nextLine();
-                    request = FilmService.LOGIN + FilmService.DELIMITER + username + FilmService.DELIMITER + password;
-                    break;
-                case "3":
-                    System.out.println("Title of film to rate: ");
-                    String rateTitle = userInput.nextLine();
-                    System.out.println("Rating out of 10: ");
-                    String rating = userInput.nextLine();
-                    request = FilmService.RATE + FilmService.DELIMITER + rateTitle + FilmService.DELIMITER + rating;
-                    break;
-                case "4":
-                    System.out.println("Enter Title: ");
-                    String searchTitle = userInput.nextLine();
-                    request = FilmService.SEARCHNAME + FilmService.DELIMITER + searchTitle;
-                    break;
-                case "5":
-                    System.out.println("Enter Genre: ");
-                    String searchGenre = userInput.nextLine();
-                    request = FilmService.SEARCHGENRE + FilmService.DELIMITER + searchGenre;
-                    break;
-                case "6":
-                    System.out.println("Enter title of Film: ");
-                    String filmTitle = userInput.nextLine();
-                    System.out.println("Enter genre of Film: ");
-                    String filmGenre = userInput.nextLine();
-                    request = FilmService.ADD + FilmService.DELIMITER + filmTitle + FilmService.DELIMITER + filmGenre;
-                    break;
-                case "7":
-                    System.out.println("Enter title of Film to remove: ");
-                    String removeTitle = userInput.nextLine();
-                    request = FilmService.REMOVE + FilmService.DELIMITER + removeTitle;
-                    break;
-                case "8":
-                    request = FilmService.SHUTDOWN;
-                    break;
-                case "9":
-                    request = FilmService.LOGOUT;
-                    break;
-                case "10":
-                    request = FilmService.EXIT;
-                    break;
-                default:
-                    System.out.println("Please select one of the options");
-                    System.out.println("=========================================================");
-                    continue;
-            }
-            valid = true;
+    /**
+     * Handles responses from the Film Server to the client, and outputs them in a clean format.
+     * Also, correctly tracks if the user is an admin.
+     *
+     * @param response
+     * @return
+     */
+    private static boolean processResponse(String response) {
+        String[] parts = response.split(FilmService.DELIMITER);
+        switch (parts[0]) {
+            case FilmService.ADDED:
+                System.out.println("User registered successfully!");
+                break;
+            case FilmService.REJECTED:
+                System.out.println("Registration failed.");
+                break;
+            case FilmService.SUCCESSADMIN:
+                System.out.println("Logged in as admin!");
+                return true;
+            case FilmService.SUCCESSUSER:
+                System.out.println("Logged in as user!");
+                return false;
+            case FilmService.FAILED:
+                System.out.println("Login failed. Please try again.");
+                break;
+            case FilmService.LOGOUTOUT:
+                System.out.println("Logged out successfully!");
+                break;
+            case FilmService.INVALIDRATING:
+                System.out.println("Invalid rating supplied.");
+                break;
+            case FilmService.NOLOGIN:
+                System.out.println("You are not logged in.");
+                break;
+            case FilmService.NOMATCH:
+                System.out.println("No match found.");
+                break;
+            case FilmService.GOODBYE:
+                System.out.println("Goodbye!");
+                break;
+            case FilmService.REMOVED:
+                System.out.println("Film removed successfully!");
+                break;
+            case FilmService.NOTFOUND:
+                System.out.println("Film not found or insufficient permissions!");
+                break;
+            case FilmService.EXISTS:
+                System.out.println("Film already exists!");
+                break;
+            case FilmService.NOPERMS:
+                System.out.println("Insufficient permissions!");
+                break;
+            case "":
+                break;
+            default:
+                System.out.println("Invalid response from the server.");
         }
-        return  request;
+        return false;
     }
 }

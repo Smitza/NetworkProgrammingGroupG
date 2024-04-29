4c043e1ca0a657862bcace055ca86c57c0a77ff3
Smitza <seansj02@gmail.com>
2024-04-29 19:57:57 +0100

    Temp Threading

--- GroupGCA/src/main/java/server/FilmServer.java
+++ GroupGCA/src/main/java/server/FilmServer.java
@@ -1,30 +1,22 @@
 package server;
 
-import film_service.business.Film;
 import film_service.business.FilmManager;
-import film_service.business.User;
 import film_service.business.UserManager;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
-
 public class FilmServer {
-
     private static final UserManager userManager = new UserManager();
     private static final FilmManager filmManager = new FilmManager();
-    private static User currentUser = null;
-
     public static void main(String[] args) {
         try (ServerSocket listeningSocket = new ServerSocket(FilmService.PORT)) {
             while (true) {
                 Socket dataSocket = listeningSocket.accept();
-                handleClientSession(dataSocket);
+                FilmClientHandler clientHandler = new FilmClientHandler(dataSocket,userManager, filmManager);
+                Thread worker = new Thread(clientHandler);
+                worker.start();
             }
         } catch (BindException e) {
             System.out.println("BindException occurred when attempting to bind to port " + FilmService.PORT);
@@ -35,222 +27,7 @@ public class FilmServer {
         }
     }
 
-    public static void handleClientSession(Socket dataSocket) {
-        try (Scanner input = new Scanner(dataSocket.getInputStream());
-             PrintWriter output = new PrintWriter(dataSocket.getOutputStream())) {
-            boolean validSession = true;
-            while (validSession) {
-                String message = input.nextLine();
-                System.out.println("Message received: " + message);
-                String[] components = message.split(FilmService.DELIMITER);
-                String response = null;
-                switch (components[0]) {
-                    case (FilmService.REGISTER):
-                        response = register(components);
-                        break;
-                    case (FilmService.LOGIN):
-                        response = login(components);
-                        break;
-                    case (FilmService.LOGOUT):
-                        response = logout(components);
-                        break;
-                    case (FilmService.RATE):
-                        response = rate(components);
-                        break;
-                    case (FilmService.SEARCHNAME):
-                        response = searchName(components);
-                        break;
-                    case (FilmService.SEARCHGENRE):
-                        response = searchGenre(components);
-                        break;
-                    case (FilmService.ADD):
-                        response = addFilm(components);
-                        break;
-                    case (FilmService.REMOVE):
-                        response = removeFilm(components);
-                        break;
-                    case (FilmService.EXIT):
-                        if(components.length == 1) {
-                            response = FilmService.GOODBYE;
-                            validSession = false;
-                        } else {
-                            response = FilmService.INVALID;
-                        }
-                        break;
-                    case (FilmService.SHUTDOWN):
-                        if(components.length == 1) {
-                            if (currentUser == null || !currentUser.isAdmin()) {
-                                response = FilmService.NOPERMS;
-                            } else {
-                                response = FilmService.SHUTTINGDOWN;
-                                dataSocket.close();
-                            }
-                        }
-                        break;
-                    default:
-                        response = FilmService.INVALID;
-                }
-                output.println(response);
-                output.flush();
-            }
-        } catch (NoSuchElementException e) {
-            System.out.println("The user sent an invalid request: ");
-            System.out.println(e.getMessage());
-        } catch (IOException e) {
-            System.out.println("An IOException occurred when trying to communicate with: " + dataSocket.getInetAddress() + ":" + dataSocket.getPort());
-            System.out.println(e.getMessage());
-        }
-    }
 
-    private static String register(String[] components) {
-        String response;
-        if (components.length != 3) {
-            response = FilmService.REJECTED;
-        } else {
-            String username = components[1];
-            String password = components[2];
-            boolean registrationResult = userManager.addUser(username, password, false);
-
-            if (registrationResult) {
-                response = FilmService.ADDED; // User registered successfully
-            } else {
-                response = FilmService.REJECTED; // Registration failed (username already exists)
-            }
-        }
-        return response;
-    }
-
-    private static String login(String[] components) {
-        String response;
-        if (components.length != 3) {
-            response = FilmService.REJECTED;
-        } else {
-            String username = components[1];
-            String password = components[2];
-            currentUser = userManager.authenticateUser(username, password);
-            if (currentUser != null) {
-                if(currentUser.isAdmin()){
-                    response = FilmService.SUCCESSADMIN;
-                } else {
-                    response= FilmService.SUCCESSUSER;
-                }
-            } else {
-                response = FilmService.FAILED;
-            }
-        }
-        return response;
-    }
-
-    private static String logout(String[] components) {
-        String response;
-        if (components.length != 1) {
-            response = FilmService.REJECTED;
-        } else {
-            currentUser = null;
-            response = FilmService.LOGOUTOUT;
-        }
-        return response;
-    }
-
-    private static String rate(String[] components) {
-        String response;
-        if (components.length < 3) {
-            response = FilmService.REJECTED;
-        } else {
-            String title = components[1];
-            try {
-                int rating = Integer.parseInt(components[2]);
-                    if (currentUser != null) {
-                        boolean rated = filmManager.rateFilm(title, rating);
-                        if (rated) {
-                            response = FilmService.SUCCESS;
-                        } else {
-                            response = FilmService.INVALIDRATING;
-                        }
-                    } else {
-                        response = FilmService.NOLOGIN;
-                    }
-            } catch (NumberFormatException e) {
-                response = FilmService.INVALIDRATING;
-            }
-        }
-        return response;
-    }
-
-    private static String searchName(String[] components) {
-        String response;
-        if (components.length != 2) {
-            response = FilmService.INVALID;
-        } else {
-            String title = components[1];
-            Film filmResult = filmManager.searchByTitle(title);
-            if (filmResult != null) {
-                response = filmResult.encode("%%");
-            } else {
-                response = FilmService.NOMATCH;
-            }
-        }
-        return response;
-    }
-
-    private static String searchGenre(String[] components) {
-        String response;
-        if (components.length != 2) {
-            response = FilmService.INVALID;
-        } else {
-            String genre = components[1];
-            List<Film> resultList = filmManager.searchByGenre(genre);
-            if (!resultList.isEmpty()) {
-                response = filmManager.encode(genre,"~~", "%%");
-            } else {
-                response = FilmService.NOMATCH;
-            }
-        }
-        return response;
-    }
-
-    private static String addFilm(String[] components) {
-        String response;
-        if (components.length != 3) {
-            response = FilmService.REJECTED;
-        } else {
-            if (currentUser == null || !currentUser.isAdmin()) {
-                response = FilmService.NOPERMS;
-            } else {
-                String title = components[1];
-                String genre = components[2];
-                Film newFilm = new Film(title, genre);
-                boolean isAdded = filmManager.addFilm(newFilm);
-                if(isAdded) {
-                    response = FilmService.ADDED;
-                } else {
-                    response = FilmService.EXISTS;
-                }
-            }
-        }
-        return response;
-    }
-
-
-    private static String removeFilm(String[] components) {
-        String response;
-        if (components.length != 2) {
-            response = FilmService.REJECTED;
-        } else {
-            if (currentUser == null || !currentUser.isAdmin()) {
-                response = FilmService.NOPERMS;
-            } else {
-                String title = components[1];
-                boolean isRemoved = filmManager.removeFilm(title);
-                if(isRemoved) {
-                    response = FilmService.REMOVED;
-                } else {
-                    response = FilmService.NOTFOUND;
-                }
-            }
-        }
-        return response;
-    }
 
 
 }
